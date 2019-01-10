0d36a7bd1644b2bf451b8e1bdca222ed7169aa01
Linus Probert <linus.probert@verisure.com>
2019-01-10 16:14:24 +0100

    Threading in the timeout server

--- python/timeoutserver.py
+++ python/timeoutserver.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 """
 Short program written to test the timeout handling of a microservice.
@@ -6,25 +7,34 @@ This script will accept a connection and then do nothing.
 """
 
 def listen(host, port):
+    threads = []
+    connectionCount = 0
+
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     connection.bind((host, port))
     connection.listen(10)
+
     while True:
-        cur_connection, address = connection.accept()
-        print("[**] Connection established from %s:%d" % address)
+        con, address = connection.accept()
+        connectionCount += 1
+        print("[**] Connection established [%d] from %s:%d" % (connectionCount, *address))
+        thread = Thread(target=handle_connection, args=(con, address, connectionCount))
+        thread.start()
+        threads.append(thread)
 
-        while True:
-            data = cur_connection.recv(2048)
-            if len(data) > 0:
-                print("[**] Received data from %s:%d:" % address)
-                print("[**] -------------- BEGIN DATA --------------")
-                print(str(data))
-                print("[**] -------------- END DATA ----------------")
-            else:
-                print("[!!] Received nothing, terminating client")
-                break
+def handle_connection(connection, address, connectionNumber):
+    while True:
+        data = connection.recv(2048)
+        if len(data) > 0:
+            print("[**][%d] Received data from %s:%d:" % (connectionNumber, *address))
+            print("[**][%d] -------------- BEGIN DATA --------------" % connectionNumber)
+            print(str(data))
+            print("[**][%d] -------------- END DATA ----------------" % connectionNumber)
+        else:
+            print("[!!][%d] Received nothing, terminating client" % connectionNumber)
+            break
 
-        cur_connection.close()
+    connection.close()
 
 listen("0.0.0.0", 7070)
