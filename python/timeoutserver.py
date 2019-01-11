80f4f035dfc08a3733326bf985da976815edb423
Linus Probert <linus.probert@verisure.com>
2019-01-11 11:46:29 +0100

    Colours!!!

--- python/timeoutserver.py
+++ python/timeoutserver.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import signal
+import termstyle as sty
 from threading import Thread
 from threading import Lock
 
@@ -15,25 +16,50 @@ thread_lock = Lock()
 
 def signal_handler(sig, frame):
     global shutdown
-    print("\nSignal (%d) caught, exiting" % sig)
+    print("")
+    fatal("Signal (%d) caught, shutting down" % sig)
     with thread_lock: shutdown = True
+    log("Shutting down %d connection(s)" % len(threads))
     for thread in threads:
         thread[1].shutdown(socket.SHUT_RDWR)
         thread[0].join()
     sys.exit(0)
+
+def log(msg, inst=0):
+    if inst > 0:
+        instance = "[%d]" % inst
+    else:
+        instance = ""
+    print("%s[**]%s%s %s%s" % (sty.yellow, instance, sty.green, msg, sty.reset))
+
+def error(msg, inst=0):
+    if inst > 0:
+        instance = "[%d]" % inst
+    else:
+        instance = ""
+    print("%s[!!]%s%s %s%s" % (sty.yellow, instance, sty.green, msg, sty.reset))
+
+def fatal(msg, inst=0):
+    if inst > 0:
+        instance = "[%d]" % inst
+    else:
+        instance = ""
+    print("%s[!!]%s%s %s%s" % (sty.red, instance, sty.yellow, msg, sty.reset))
 
 def listen(host, port):
     connectionCount = 0
 
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    log("Listening on %s:%d" % (host, port))
     connection.bind((host, port))
     connection.listen(10)
 
     while True:
         con, address = connection.accept()
         connectionCount += 1
-        print("[**] Connection established [%d] from %s:%d" % (connectionCount, *address))
+        log("Connection established from %s:%d" % address, connectionCount)
         thread = Thread(target=handle_connection, args=(con, address, connectionCount))
         thread.start()
         threads.append((thread, con))
@@ -42,16 +68,16 @@ def handle_connection(connection, address, connectionNumber):
     while True:
         data = connection.recv(2048)
         if len(data) > 0:
-            print("[**][%d] Received data from %s:%d:" % (connectionNumber, *address))
-            print("[**][%d] -------------- BEGIN DATA --------------" % connectionNumber)
+            log("Received data from %s:%d:" % address, connectionNumber)
+            log("-------------- BEGIN DATA --------------", connectionNumber)
             print(str(data))
-            print("[**][%d] -------------- END DATA ----------------" % connectionNumber)
+            log("-------------- END DATA ----------------", connectionNumber)
         else:
             global shutdown
             if not shutdown:
-                print("[!!][%d] Received nothing, terminating connection" % connectionNumber)
+                error("Received nothing, terminating connection", connectionNumber)
             else:
-                print("[!!][%d] Terminating connection" % connectionNumber)
+                error("Terminating connection", connectionNumber)
             break
 
     connection.close()
